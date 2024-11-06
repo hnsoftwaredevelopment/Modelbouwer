2f6e8fdc7fb4d48dfeb23baabf06b3908749ea13
Herbie68 <herbert.nijkamp@gmail.com>
2024-11-06 17:09:46 +0100

    Started implementation Project Maintanace Page #12

--- Modelbouwer/View/ProjectManagement.xaml.cs
+++ Modelbouwer/View/ProjectManagement.xaml.cs
@@ -9,4 +9,33 @@ public partial class ProjectManagement : Page
 	{
 		InitializeComponent();
 	}
+
+	#region Checkbox for project completion is checked/unchecked
+	private void ProjectStatus( object sender, RoutedEventArgs e )
+	{
+		if ( ProjectClosed.IsChecked == true )
+		{
+			valueShow.IsChecked = false;
+		}
+		else
+		{
+			valueShow.IsChecked = true;
+		}
+	}
+	#endregion
+
+	private void ImageRotate( object sender, RoutedEventArgs e )
+	{
+
+	}
+
+	private void ImageDelete( object sender, RoutedEventArgs e )
+	{
+
+	}
+
+	private void ImageAdd( object sender, RoutedEventArgs e )
+	{
+
+	}
 }
